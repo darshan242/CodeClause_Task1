daa8a1e9740f191171ba766aa33cb675b2278642
darshan242 <89136314+darshan242@users.noreply.github.com>
2023-04-22 10:44:06 +0530

    Add files via upload

---
+++ PDFBreaker/src/pdfbreaker/PDFBreaker.java
@@ -0,0 +1,17 @@
+package pdfbreaker;
+
+/**
+ *
+ * @author DARSHAN
+ */
+public class PDFBreaker {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Dashboard db=new Dashboard();db.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
